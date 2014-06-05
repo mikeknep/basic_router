05a2cf81d427e0c4a8ecaf0c1a64f82600515f62
Mike Knepper <michaelrknepper@gmail.com>
2014-06-05 13:35:34 -0500

    Adds tests for Dispatcher
    The behavior of the Dispatcher is now under test.

---
+++ test/DispatcherTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DispatcherTest {
+    @Test
+    public void itRoutesBadRequestToBadRequestResponseBuilder() {
+        assertEquals(BadRequestResponseBuilder.class, Dispatcher.setResponseBuilder("", "", "").getClass());
+    }
+
+    @Test
+    public void itRoutesOptionsRequestToOptionsResponseBuilder() {
+        assertEquals(OptionsResponseBuilder.class, Dispatcher.setResponseBuilder("public/", "OPTIONS", "/").getClass());
+    }
+
+    @Test
+    public void itRoutesDirectoryRequestToDirectoryResponseBuilder() {
+        assertEquals(DirectoryResponseBuilder.class, Dispatcher.setResponseBuilder("public/", "GET", "/").getClass());
+    }
+
+    @Test
+    public void itRoutesFileRequestToFileResponseBuilder() {
+        assertEquals(FileResponseBuilder.class, Dispatcher.setResponseBuilder("public/", "GET", "/mock.html").getClass());
+    }
+
+    @Test
+    public void itRoutesMissingResourceRequestToMissingResourceResponseBuilder() {
+        assertEquals(MissingResourceResponseBuilder.class, Dispatcher.setResponseBuilder("public/", "GET", "/not_here.html").getClass());
+    }
+}
