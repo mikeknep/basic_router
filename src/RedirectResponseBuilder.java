475a285c979d3701f51b49d3266b3c1a1654224f
Mike Knepper <michaelrknepper@gmail.com>
2014-06-05 13:26:51 -0500

    Adds basic Redirect response builder
    The RedirectResponseBuilder sends a 301 redirect informing the client
    where to go next. Very basic at the moment.

---
+++ src/RedirectResponseBuilder.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+/**
+ * Created by mrk on 6/5/14.
+ */
+public class RedirectResponseBuilder implements ResponseBuilder {
+    public String getStatus() {
+        return "301 Moved Permanently";
+    }
+
+    public HashMap<String, String> getHeaders() {
+        HashMap<String, String> headers = new HashMap<String, String>();
+        headers.put("Location", "/");
+        return headers;
+    }
+
+    public byte[] getBody() {
+        return "This page has moved.".getBytes();
+    }
+}
---
+++ test/RedirectResponseBuilderTest.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class RedirectResponseBuilderTest {
+    RedirectResponseBuilder builder;
+    @Before
+    public void instantiateBuilder() {
+        builder = new RedirectResponseBuilder();
+    }
+
+    @Test
+    public void itReturns301RedirectStatus() {
+        assertEquals("301 Moved Permanently", builder.getStatus());
+    }
+
+    @Test
+    public void itSetsLocationInHeaders() {
+        HashMap<String, String> expectedHeaders = new HashMap<String, String>();
+        expectedHeaders.put("Location", "/");
+    }
+
+    @Test
+    public void itProvidesABasicBody() {
+        assertArrayEquals("This page has moved.".getBytes(), builder.getBody());
+    }
+}
