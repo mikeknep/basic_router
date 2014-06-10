3cabee47c33ac35f3a2394cdb6a0d1972b736df9
Mike Knepper <michaelrknepper@gmail.com>
2014-06-10 17:10:57 -0500

    Adds Auth/ProtectedResource builder
    This builder generates a 401 Unathorized response with the appropriate
    Authorize header to generate the username/password dropdown in the
    browser. The correct user/pw are defined in cobspec.

---
+++ src/com/mikeknep/basic_router/builders/ProtectedResourceResponseBuilder.java
@@ -0,0 +1,63 @@
+package com.mikeknep.basic_router.builders;
+
+import static javax.xml.bind.DatatypeConverter.parseBase64Binary;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Created by mrk on 6/10/14.
+ */
+public class ProtectedResourceResponseBuilder implements ResponseBuilder {
+    private String rootDirectory;
+    private String requestedResource;
+    private HashMap<String, String> headers;
+
+    public ProtectedResourceResponseBuilder(String rootDirectory, String requestedResource, HashMap<String, String> headers) {
+        this.rootDirectory = rootDirectory;
+        this.requestedResource = requestedResource;
+        this.headers = headers;
+    }
+
+    public String getStatus() {
+        if (validCredentials()) {
+            return "200 OK";
+        } else {
+            return "401 Unauthorized";
+        }
+    }
+
+    public HashMap<String, String> getHeaders() {
+        HashMap<String, String> headers = new HashMap<String, String>();
+        if (!validCredentials()) {
+            headers.put("WWW-Authenticate", "Basic realm=Dahomey");
+        }
+        return headers;
+    }
+
+    public byte[] getBody() {
+        if (validCredentials()) {
+            try {
+                return Files.readAllBytes(Paths.get(rootDirectory + requestedResource));
+            } catch (IOException e) {
+                return "".getBytes();
+            }
+        } else {
+            return "".getBytes();
+        }
+    }
+
+    private boolean validCredentials() {
+        try {
+            String rawCredsValue = headers.get("Authorization");
+            String encoded = rawCredsValue.replace("Basic ", "");
+            byte[] decoded = parseBase64Binary(encoded);
+
+            return (Arrays.equals("admin:hunter2".getBytes(), decoded));
+        } catch (NullPointerException e) {
+            return false;
+        }
+    }
+}
---
+++ test/com/mikeknep/basic_router/builders/ProtectedResourceResponseBuilderTest.java
@@ -0,0 +1,87 @@
+package com.mikeknep.basic_router.builders;
+
+import static javax.xml.bind.DatatypeConverter.printBase64Binary;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class ProtectedResourceResponseBuilderTest {
+    ProtectedResourceResponseBuilder noCredsBuilder;
+    ProtectedResourceResponseBuilder badCredsBuilder;
+    ProtectedResourceResponseBuilder goodCredsBuilder;
+    @Before
+    public void instantiateBuilder() {
+        String rootDirectory = "public/";
+        String requestedResource = "/logs";
+        HashMap<String, String> noCreds = new HashMap<String, String>();
+        HashMap<String, String> badCreds = new HashMap<String, String>();
+        HashMap<String, String> goodCreds = new HashMap<String, String>();
+
+        badCreds.put("Authorization", "foo:bar");
+
+        String auth = printBase64Binary("admin:hunter2".getBytes());
+        goodCreds.put("Authorization", "Basic " + auth);
+
+        noCredsBuilder = new ProtectedResourceResponseBuilder(rootDirectory, requestedResource, noCreds);
+        badCredsBuilder = new ProtectedResourceResponseBuilder(rootDirectory, requestedResource, badCreds);
+        goodCredsBuilder = new ProtectedResourceResponseBuilder(rootDirectory, requestedResource, goodCreds);
+    }
+
+    @Test
+    public void itGenerates401CodeWhenNoCredentials() {
+        assertEquals("401 Unauthorized", noCredsBuilder.getStatus());
+    }
+
+    @Test
+    public void itGenerates401WhenBadCredentials() {
+        assertEquals("401 Unauthorized", badCredsBuilder.getStatus());
+    }
+
+    @Test
+    public void itGenerates200WhenGoodCredentials() {
+        assertEquals("200 OK", goodCredsBuilder.getStatus());
+    }
+
+
+    @Test
+    public void itReturnsAuthorizationHeaderWhenNoCredentials() {
+        HashMap<String, String> expectedHeaders = new HashMap<String, String>();
+        expectedHeaders.put("WWW-Authenticate", "Basic realm=Dahomey");
+
+        assertEquals(expectedHeaders, noCredsBuilder.getHeaders());
+    }
+
+    @Test
+    public void itReturnsAuthorizationHeaderWhenBadCredentials() {
+        HashMap<String, String> expectedHeaders = new HashMap<String, String>();
+        expectedHeaders.put("WWW-Authenticate", "Basic realm=Dahomey");
+
+        assertEquals(expectedHeaders, badCredsBuilder.getHeaders());
+    }
+
+    @Test
+    public void itReturnsEmptyHeadersWhenGoodCredentials() {
+        HashMap<String, String> expectedHeaders = new HashMap<String, String>();
+
+        assertEquals(expectedHeaders, goodCredsBuilder.getHeaders());
+    }
+
+
+    @Test
+    public void itReturnsEmptyBodyWhenNoCredentials() {
+        assertArrayEquals("".getBytes(), noCredsBuilder.getBody());
+    }
+
+    @Test
+    public void itReturnsEmptyBodyWhenBadCredentials() {
+        assertArrayEquals("".getBytes(), badCredsBuilder.getBody());
+    }
+
+    @Test
+    public void itReturnsFileBodyWhenGoodCredentials() {
+        assertArrayEquals("These are secret logs.".getBytes(), goodCredsBuilder.getBody());
+    }
+}
